1235ff5159495ed4ca2c0082c4871f9cea36cd4d
bikash <vikashdash004@gmail.com>
2021-10-20 21:17:51 +0530

    connacting state data

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
 
-export default class Stack extends Component {
-    render() {
+ class Stack extends Component {
+     render() {
+         const {title,cards} = this.props.stackData;
+
         return (
             <div>
                 <Link to="/">Home</Link>
-                Stack
+                <h1>{title}</h1>
+                <br/>
+                {
+                    cards.map((card)=>{
+                        return (
+                            <div key={card.id}>{card.prompt}</div>
+                        )
+                    })
+                }
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {stackData:state}
+}
+
+export default connect(mapStateToProps)(Stack)
