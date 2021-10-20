7dfad20994242a6fc969894e3830351aa5aa5cdf
bikash <vikashdash004@gmail.com>
2021-10-20 20:54:23 +0530

    dispatch action

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import Stacks from '../data/stacks.json'
 import { Link } from 'react-router-dom'
-
-export default class StackList extends Component {
+import {setStack} from '../actions';
+import {connect} from 'react-redux'
+class StackList extends Component {
     render() {
+        console.log('stack list props',this.props);
         return (
             <div>
                 {
                     Stacks.map((stack,index)=>{
                         return(
-                            <Link to='/stack/' key={index} ><h4 >{stack.title}</h4></Link>
+                            <Link to='/stack/' 
+                            onClick={(e)=>this.props.setStack(stack)} 
+                            key={index} ><h4 >{stack.title}</h4></Link>
                         );
                     })
                 }
@@ -17,3 +21,7 @@ export default class StackList extends Component {
         )
     }
 }
+
+
+
+export default connect(null,{setStack})(StackList);
